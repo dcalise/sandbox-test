fad63f4271b2e2daf42fb3cb1881f35b990634d6
Dan Calise <me@dancali.se>
2020-07-08 21:57:06 -0400

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "../css/App.css";
+// import components here
+
+function App() {
+  return <div className="app">Trivia! testing</div>;
+}
+
+export default App;
